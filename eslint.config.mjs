2ff23fc7b2424906e13b433d8e70bd111b7a748e
eapopova <e.popova@dunice.net>
2025-03-03 15:24:10 +0300

    Configured sequelize and DB

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,6 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
@@ -29,7 +28,14 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'semi': ['error', 'always'],
+    },
+  },
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'prettier/prettier': 'error',
     },
   },
 );
